bbd738c8fd4e58effdc52fd2871ab80205f36c55
daferso93 <daferso93@gmail.com>
2017-02-19 21:31:54 -0600

    Commit 2, assets change | Daniel Fernandez Solano

---
+++ Assets/Scripts/mainPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainPlayer : MonoBehaviour {
+
+	public float moveSpeed;
+	public float jumpForce;
+
+	public KeyCode left;
+	public KeyCode right;
+	public KeyCode jump;
+	public KeyCode throwBall;
+
+	private Rigidbody2D theRB;
+
+	// Use this for initialization
+	void Start () {
+		theRB = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (left)) {
+			theRB.velocity = new Vector2 (-moveSpeed, theRB.velocity.y);
+		} else if (Input.GetKey (right)) {
+			theRB.velocity = new Vector2 (moveSpeed, theRB.velocity.y);
+		}
+	}
+}
